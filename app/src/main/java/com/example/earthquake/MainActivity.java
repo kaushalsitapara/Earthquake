f0d26b35332e086c9ab9298304fbb0d57bb968a2
Kaushal <ksitapara8@gmail.com>
2019-06-07 23:49:02 +0530

    lesson one competed with reworked code
    This is a restart for BarKart so, I can Improve my concept of Android Studio which I had learned in 6 hours at Gujarat Hackathon.
    In lession I have learned about various classes related to JSON for java.

---
+++ app/src/main/java/com/example/earthquake/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.earthquake;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
+
+
+        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+
+        final EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
+
+        earthquakeListView.setAdapter(adapter);
+
+        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                Earthquake currentEarthquake = adapter.getItem(position);
+
+
+                Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
+
+
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+
+                startActivity(websiteIntent);
+            }
+        });
+    }
+}
